a539e1cc7ae1328802017b6c6dddd3a3610fe464
Kola <kolawole.campbell@gmail.com>
2019-11-29 19:48:19 -0500

    bugs resolved

--- PRP.java
+++ PRP.java
@@ -3,13 +3,14 @@ public class PRP{
     public static void main(String[] args) {
         lexicon L = new lexicon();
         lexicon.HashCreate(L,15);
-        lexicon.HashEmpty(L);
+        
         lexicon.HashInsert(L, "yer");
         lexicon.HashInsert(L, "eyr");
-        lexicon.HashInsert(L, "rey");
-        lexicon.HashSearch(L, "rey");
+        lexicon.HashInsert(L, "yer");
         lexicon.HashDelete(L, "yer");
-        lexicon.HashSearch(L, "yer");
+        lexicon.HashDelete(L, "yer");
+        lexicon.HashDelete(L, "y");
+        lexicon.HashEmpty(L);
         return;
     }
     
--- lexicon.java
+++ lexicon.java
@@ -2,7 +2,7 @@
 public class lexicon {
     char[] A = null;
     hashT[] hashTable;
-    
+
     public static class hashT {
         private Integer key;
         private Integer value;
@@ -26,25 +26,26 @@ public class lexicon {
         int asciiW = 0;
 
         for (int i = 0; i < x.length; i++) {
-            asciiW = asciiW + (int)x[i];
+            asciiW = asciiW + (int) x[i];
         }
         return asciiW;
 
     }
-    private static int hashIndex(int asciiW, int iteration, int length){
-        int index = (asciiW+(iteration*iteration))%length;
+
+    private static int hashIndex(int asciiW, int iteration, int length) {//single step of hash function with an iteration parameter for search operation
+        int index = (asciiW + (iteration * iteration)) % length;
         return index;
     }
-    private static int hashProb(lexicon L, int hash, int size) {
-        int index=hash% size;
+
+    private static int hashProb(lexicon L, int hash, int size) {//quadratic probing for insert operation
+        int index = hash % size;
         int i = 0;
-        while(L.hashTable[index]!=null){
-            index = (hash + (i*i)) % size;
+        while (L.hashTable[index] != null) {
+            index = (hash + (i * i)) % size;
             i++;
         }
         return index;
     }
-
 
     public static void HashCreate(lexicon L, int m) {
         L.A = new char[15 * m];
@@ -73,15 +74,15 @@ public class lexicon {
         char[] word = wordS.toCharArray();
         int asciiW = hash(word);
         int start = 0;
-        while (L.A[start] + L.A[start+1] != 0) {
+        while (L.A[start] + L.A[start + 1] != 0) {
             start++;
         }
         if (start != 0)
-            start = start +1;
+            start = start + 1;
 
         for (int i = start; i < word.length + start; i++)
             L.A[i] = word[i - start];
-        int key = hashProb(L,asciiW, L.hashTable.length);
+        int key = hashProb(L, asciiW, L.hashTable.length);
         L.hashTable[key] = new hashT(key, start);
     }
 
@@ -89,26 +90,34 @@ public class lexicon {
         char[] word = wordS.toCharArray();
         int asciiW = hash(word);
 
-        int key = hashIndex(asciiW,0, L.hashTable.length);
+        int key = hashIndex(asciiW, 0, L.hashTable.length);
         try {
             int j = 0;
-            while (L.hashTable[key] != null && key < L.hashTable.length) {
+            while (key < L.hashTable.length) {// iterate through hashtable using HashIndex
                 boolean found = true;
                 int i = 0;
-                while (L.A[L.hashTable[key].value + i] != 0) {
-                    found = found && (word[i] == L.A[L.hashTable[key].value + i]);
-                    i++;
-                }
-                if (found) {
-                    System.out.println(wordS + " was found in slot " + key);
-                    return key;
-                }
+                if (L.hashTable[key] != null) {// if computed hashtable index is not null check if word in that slot is the key we need
+                    while (L.A[L.hashTable[key].value + i] != 0) {
+                        found = found && (word[i] == L.A[L.hashTable[key].value + i]);
+                        i++;
+                    }
+                    if (found) {
+                        System.out.println(wordS + " was found in slot " + key);
+                        return key;
+                    }
 
-                else {
+                    else {
+                        j++;
+                        key = hashIndex(asciiW, j, L.hashTable.length);
+
+                    }
+                } else {//this if-else allows iteration to continue even if a previous hash index is null in case of deletion
                     j++;
-                key = hashIndex(asciiW, j, L.hashTable.length);
-
+                    if(j*j > L.hashTable.length)//prevent from restarting probings
+                    break;
+                    key = hashIndex(asciiW, j, L.hashTable.length);
                 }
+
             }
             System.out.println(wordS + " was not found");
 
@@ -121,12 +130,11 @@ public class lexicon {
 
     public static void HashDelete(lexicon L, String wordS) {
         int key = HashSearch(L, wordS);
-        if(key != -1){
-        L.hashTable[key] = null;
-        System.out.println(wordS + " deleted.");
-    }
-        else
-        System.out.println(wordS + " not found.");
+        if (key != -1) {
+            L.hashTable[key] = null;
+            System.out.println(wordS + " deleted.");
+        } else
+            System.out.println(wordS + " not found.");
     }
 
 }
